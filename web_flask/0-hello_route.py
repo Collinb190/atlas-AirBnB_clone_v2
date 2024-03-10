c7821e493fc73a46ed86d359bfbf10edfdc8df95
Collinb190 <collinb190@gmail.com>
2024-03-10 10:15:10 -0500

    pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,10 +8,12 @@ from flask import Flask
 """ Name tells to look for resources."""
 app = Flask(__name__)
 
+
 """ Decorator to tell Flask what URL should trigger our function. """
 @app.route('/', strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
+
 
 """ Prevents the Flask development server from starting if the script is imported elsewhere. """
 if __name__=='__main__':
