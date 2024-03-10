8bd576a2b60186669e5e400c6472861a1aa0122a
Collinb190 <collinb190@gmail.com>
2024-03-10 11:28:58 -0500

    task1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This is a script that starts a Flask web application with two routes.
+"""
+from flask import Flask
+
+
+# Name tells to look for resources.
+app = Flask(__name__)
+
+
+# Decorator to tell Flask what URL should trigger our function.
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+# Decorator to tell Flask what URL should trigger our function.
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return'HBNB'
+
+
+# Prevents the Flask dev server from starting if imported elsewhere.
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
