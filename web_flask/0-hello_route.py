5fbc0474108174ece0fb22f42272c0b07bc8f9d9
Collinb190 <collinb190@gmail.com>
2024-03-10 10:25:14 -0500

    pep8 -_-

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,16 +5,16 @@ This is a script that starts a Flask web application.
 from flask import Flask
 
 
-""" Name tells to look for resources."""
+# Name tells to look for resources.
 app = Flask(__name__)
 
 
-""" Decorator to tell Flask what URL should trigger our function. """
+# Decorator to tell Flask what URL should trigger our function.
 @app.route('/', strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
 
 
-""" Prevents the Flask development server from starting if the script is imported elsewhere. """
-if __name__=='__main__':
+# Prevents the Flask dev server from starting if imported elsewhere.
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
